2b539fddca11b5360205a2110ef2cf1679180458
Andrew Edwards <ae0000@gmail.com>
2011-07-05 09:39:25 +1000

    Adding api files

---
+++ Eventarc.php
@@ -0,0 +1,439 @@
+<?php
+/**
+ * 
+ *
+ * @package    EventarcApiPhpLibrary
+ * @category   Library
+ * @author     Eventarc
+ * @copyright  (c) 2009-2011 Eventarc
+ * @license    http://api.eventarc.com/doc/license.txt
+ */
+class Eventarc
+{
+	const VERSION = 2.0;
+
+	// TODO A little documentation....
+	protected $params = array();
+	private $server = 'https://api.eventarc.com/api/v2/';
+	protected $method;
+	public $response;
+	public $error;
+	public $u_id;
+	public $u_name;
+	public $u_apikey;
+	public $last_payload;
+	public $last_json_payload;
+	public $history;
+	public $keep_history = FALSE;
+
+	/**
+	 * Constructor
+	 */
+	public function __construct($u_apikey=FALSE, $u_id=FALSE)
+	{
+		if (is_numeric($u_id))
+		{
+			$this->u_id = $u_id;
+		}
+		elseif ($u_id)
+		{
+			$this->u_name = $u_id;
+		}
+		$this->u_apikey = $u_apikey;
+	}
+
+	public function group_list($u_id=FALSE)
+	{
+		if ( ! $u_id)
+		{
+			$u_id = $this->u_id;
+		}
+		return $this->call('eventarc.group.list', array(
+			'u_id' => $u_id
+			)
+		);
+	}
+
+	public function event_list($e_status=FALSE)
+	{
+		if ($e_status)
+		{
+			$this->format_params(array('e_status' => $e_status));
+		}
+		return $this->call('eventarc.event.list');
+	}
+
+	public function group_get($g_id)
+	{
+		return $this->call('eventarc.group.get', array(
+			'g_id' => $g_id
+			)
+		);
+	}
+	
+	public function group_create(array $params)
+	{
+		// If g_parent is not set, set default to 0 (the first group)
+		if ( ! array_key_exists('g_parent', $params))
+		{
+			$params['g_parent'] = 0;
+		}	
+		return $this->call('eventarc.group.create', $params);
+	}
+
+	public function user_get($u_id=FALSE)
+	{
+		if ( ! $u_id)
+		{
+			$u_id = $this->u_id;
+		}
+		return $this->call('eventarc.user.get', array(
+			'u_id' => $u_id
+			)
+		);
+	}
+
+	public function add_event(array $event_data)
+	{
+		$this->format_params($event_data);
+		return $this;
+	}
+
+	public function add_address(array $address_data)
+	{
+		$this->format_params($address_data);
+		return $this;
+	}
+
+	public function add_widget(array $widget_data)
+	{
+		$this->add_array_item('wd_data', $widget_data);
+		return $this;
+	}
+
+	public function add_ticket(array $ticket_data)
+	{
+		$this->add_array_item('t_data', $ticket_data);
+		return $this;
+	}
+
+	public function add_theme(array $theme_data)
+	{
+		$this->format_params($theme_data);
+		return $this;
+	}
+
+	public function event_create($params=FALSE)
+	{
+		return $this->call('eventarc.event.create', $params);
+	}
+
+	public function event_copy($e_id)
+	{
+		return $this->call('eventarc.event.copy', array('e_id' => $e_id));
+	}
+
+	public function add_ticket_limit($to_total)
+	{
+		$this->format_params(array('to_total' => $to_total));
+		return $this;
+	}
+	
+	public function add_ticket_show_fees($show_fees=FALSE)
+	{
+		$this->format_params(array('to_showfees' => ($show_fees)?1:0));
+		return $this;
+	}
+
+
+	/**
+	 * Call a eventarc API method
+	 * 
+	 * @param mixed $method 
+	 * @param mixed $data 
+	 * @access public
+	 * @return void
+	 */
+	public function call($method_in, $data = FALSE)
+	{
+		if ( ! is_string($method_in))
+		{
+			// The method must be a string
+			throw new Eventarcapi_Exception(
+				'Invalid method called. Try using a string.', 123);
+		}
+		
+		$this->method = $method_in;
+
+		// Format the params if any given
+		if ( ! is_array($data))
+		{
+			$data = array();
+		}
+
+		// Add the $data to the params
+		$this->format_params($data);	
+
+		// Call the api
+		return $this->send_payload();
+	}
+
+	private function add_array_item($key, array $item)
+	{
+		if ( ! array_key_exists($key, $this->params))
+		{
+			$this->params[$key] = array();
+		}
+		$this->params[$key][] = $item;
+	}
+
+	/**
+	 * Take the given data and convert it into the correct format. The basic
+	 * syntax is to put variables into their own 'data' array.
+	 *
+	 * eg. u_forename would go in the u_data array.
+	 *
+	 * If there is a digit as the first character then that is indicating that
+	 * the variables are par tof an array.
+	 *
+	 * eg. 0_t_name, 1_t_name, 2_t_name would result in:
+	 * t_data = array ( array (t_name), array(t_name), array(t_name))
+	 *
+	 * If there is a error with an item from the array (ie. it does not have a
+	 * underscore to indicate its data array), then it is put in at the root of
+	 * the array as is. (GIGO)
+	 *
+	 * Once the data has been converted it is added to $this->params
+	 *
+	 * @param array $data 
+	 * @access private
+	 * @return void
+	 */
+	private function format_params(array $data)
+	{
+		foreach ($data as $key=>$value)
+		{
+			// All keys are lowercase
+			$key = strtolower($key);
+
+			// Split key on underscore
+			$split = explode('_', $key);
+
+			// Check for a key that is not an array or data set
+			if (count($split) === 1)
+			{
+				// This item is not an array or in a data array, so send it as
+				//  it is
+				$this->params[$key] = $value;
+				continue;
+			}
+
+			// Check for a a-z character(s) which indicates we are making a data
+			//  array for this key
+			// eg. t_name
+			if (ctype_lower($split[0]))
+			{
+				$data_name = $split[0].'_data';
+
+				// Make a data array for this key, but check to see if one
+				//  already exists
+				if ( ! array_key_exists($data_name, $this->params))
+				{
+					$this->params[$data_name] = array();
+				}
+
+				// Add the data
+				$this->params[$data_name][$key] = $value;
+				continue;
+			}
+
+			// Check for an array
+			// eg. 5_t_name
+			if (count($split) === 3 
+				AND ctype_digit($split[0]) 
+				AND ctype_lower($split[1]))
+			{
+				// Check if the key already exists
+				$data_name = $split[1].'_data';
+				$index = $split[0];
+				$new_key = $split[1].'_'.$split[2];
+
+				if ( ! array_key_exists($data_name, $this->params))
+				{
+					$this->params[$data_name] = array();
+				}
+
+				// Check to see if the array item already exists
+				if ( ! isset($this->params[$data_name][$index]))
+				{
+					$this->params[$data_name][$index] = array();
+				}
+				
+				$this->params[$data_name][$index][$new_key] = $value;
+			}
+		}
+
+		// If they have set the api key and u_id (in the constructor or
+		//  directly) then add these to the params too
+		if ($this->u_id OR $this->u_name OR $this->u_apikey)
+		{
+			// Do not overwrite them
+			if ( ! array_key_exists('u_data', $this->params))
+			{
+				$this->params['u_data'] = array();
+			}
+
+			if ($this->u_id AND ! array_key_exists('u_id', $this->params['u_data']))
+			{
+				$this->params['u_data']['u_id'] = $this->u_id;
+			}
+
+			if ($this->u_name AND ! array_key_exists('u_name', $this->params['u_data']))
+			{
+				$this->params['u_data']['u_name'] = $this->u_name;
+			}
+
+			if ($this->u_apikey AND ! array_key_exists('u_apikey', $this->params['u_data']))
+			{
+				$this->params['u_data']['u_apikey'] = $this->u_apikey;
+			}
+		}
+	}
+
+
+	private function send_payload()
+	{
+		// Prepare the payload
+		$payload = array(
+			'jsonrpc' => '2.0',
+			'method' => $this->method,
+			'id' => time(),
+			'params' => $this->params
+		);
+
+		// Convert payload to JSON
+		if (($json_payload = json_encode($payload)) === NULL)
+		{
+			// JSON encode failed
+			throw new Eventarcapi_Exception(
+				'We were unable to encode the data into JSON', 123);
+		}
+
+		// If we are recording the history, then... record it
+		if ($this->keep_history)
+		{
+			$this->history[$this->method] = array(
+				'method'		=> $this->method,
+				'params'		=> $this->params,
+				'payload'		=> $payload,
+				'json_payload'	=> $json_payload
+			);
+		}
+
+		// Save the payload(s)
+		$this->last_payload = $payload;
+		$this->last_json_payload = $json_payload;
+
+		// Wipe the params
+		$this->params = array();
+
+		// Send the payload and wait for a response
+		$ch = curl_init();
+
+		// Set URL and other appropriate options
+		curl_setopt($ch, CURLOPT_URL, $this->server);
+		curl_setopt($ch, CURLOPT_POST, TRUE);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $json_payload);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Expect:'));
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($ch, CURLOPT_FILETIME, TRUE);
+		
+		// Grab URL, and print
+		$response = curl_exec($ch);
+	
+		if (curl_errno($ch) > 0)
+		{
+			// Error
+			throw new Eventarcapi_Exception(
+				'Failed to contact server: '.print_r(curl_error($ch), TRUE),
+				123);
+		}
+
+		curl_close($ch);
+
+		// Process the response
+		return $this->process_response($response);
+	}
+
+	private function process_response($response)
+	{
+		// The response should be valid JSON
+		$decoded = json_decode($response, TRUE);
+
+		// Check for JSON errors
+		switch (json_last_error())
+		{
+			case JSON_ERROR_NONE:
+				// The JSON is valid
+				$this->response = $decoded;
+				break;
+			case JSON_ERROR_DEPTH:
+				throw new Eventarcapi_Exception(
+					'JSON nested too deep', 400);
+				break;
+			case JSON_ERROR_CTRL_CHAR:
+				throw new Eventarcapi_Exception(
+					'Invalid control character in JSON', 400);
+				break;
+			case JSON_ERROR_SYNTAX:
+			var_dump($this->history);
+			die($response);
+				throw new Eventarcapi_Exception(
+					'Invalid JSON syntax', 400);
+				break;
+			case JSON_ERROR_UTF8:
+				throw new Eventarcapi_Exception(
+					'Incorrect character encoding', 400);
+				break;
+			default:
+				throw new Eventarcapi_Exception(
+					'Unknown JSON error', 400);
+				break;
+		}
+
+		// If we are recording the history, then... record it
+		if ($this->keep_history)
+		{
+			$this->history[$this->method]['response'] = $decoded;
+			$this->history[$this->method]['json_response'] = $response;
+		}
+
+		// Check to see if the request was successful
+		if (array_key_exists('error', $this->response))
+		{
+			// Store the error
+			$this->error = $this->response['error'];
+			
+			// There was an error, throw an exception
+			throw new Eventarcapi_Exception(
+				$this->response['error']['message'],
+				$this->response['error']['code']);
+		}
+
+		// We should now have a result object with the results within
+		if ( ! array_key_exists('result', $this->response))
+		{
+			// The JSON is not valid as there either has to be a error object or
+			//  a result object
+			throw new Eventarcapi_Exception(
+				'The response was not valid JSON-RPC.', 123);
+		}
+
+		// Return the result
+		return $this->response['result'];
+	}
+}
+
+class Eventarcapi_Exception extends Exception {}
---
+++ examples/index.php
@@ -0,0 +1,447 @@
+<?php
+
+/**
+ * Eventarc api example file
+ *
+ * You will need a valid username and password for these examples to work
+ *
+ * These examples will show:
+ * - Getting information about the user (you)
+ * - Getting a list of the groups
+ * - Creating a group
+ * - Creating a simple event
+ * - Getting a list of events
+ * - Getting a list of events for a group
+ * - Editing an event
+ *
+ */
+
+// !! NOTE
+// You need to replace these with your own values or the api will be very upset
+$u_apikey = '8de323378f4427ec9b38'; // 'd24a754b11e269159873';
+$u_id = 3;
+$u_name = 'testuser';
+
+// We will use the following variables for various api calls. They will be set
+//  from the results of other api calls.
+$g_id = FALSE; // Group id
+$e_id = FALSE; // Event id
+$g_data = FALSE; // Group data
+
+// Get a copy of eventarcapi
+$eventarc = new Eventarc($u_apikey, $u_id);
+
+// Tell the api to store the history for the calls we are making
+$eventarc->keep_history = TRUE;
+
+try {
+	$result = $eventarc->event_list('pending');
+	// The result will contain the user data
+	// var_dump($result);
+}
+catch (Eventarcapi_Exception $e)
+{
+	// Check $eventarc->error to see why this failed
+	// var_dump($e);
+}	
+
+
+try {
+	// The result will contain the user data
+	// var_dump($result);
+}
+catch (Eventarcapi_Exception $e)
+{
+	// Check $eventarc->error to see why this failed
+	// var_dump($e);
+}	
+/**
+ * EXAMPLE eventarc.user.get
+ * 
+ * Get all the data available for ourselves
+ *
+ */
+
+// Because we set the u_id and u_apikey in the constructor, we don't need to
+//  worry about adding them in here as they are auto added.
+$params = array();
+
+try {
+	$result = $eventarc->call(
+		'eventarc.user.get',
+		$params);
+	$result = $eventarc->user_get();
+
+	// The result will contain the user data
+	// var_dump($result);
+}
+catch (Eventarcapi_Exception $e)
+{
+	// Check $eventarc->error to see why this failed
+	// var_dump($e);
+}	
+
+/**
+ * EXAMPLE eventarc.group.list
+ * 
+ * Get all of our groups
+ *
+ */
+
+// Because we set the u_id and u_apikey in the constructor, we don't need to
+//  worry about adding them in here as they are auto added.
+$params = array();
+
+try {
+	$result = $eventarc->call(
+		'eventarc.group.list',
+		$params);
+	$result = $eventarc->group_list();
+
+	// The result will contain the group list
+	// Lets store the first group for using in later api calls
+	$g_id = $result[0]['g_id'];
+}
+catch (Eventarcapi_Exception $e)
+{
+	// Check $eventarc->error to see why this failed
+	// var_dump($e);
+}	
+
+/**
+ * EXAMPLE eventarc.group.get
+ * 
+ * Get the details of the first group to come through from the previous api call
+ *
+ */
+
+// We need to send the group id for the group we want to get. It needs to be
+//  within a 'g_data' array. However the eventarcapi will automatically put any
+//  variable that is prefixed with a character and underscore into the correct
+//  data array. ie. u_name is put into u_data as u_data['u_id'] = 123;
+$params = array('g_id' => $g_id);
+
+try {
+	$result = $eventarc->call(
+		'eventarc.group.get',
+		$params);
+	$result = $eventarc->group_get($g_id);
+
+	// The result will contain the group information
+	$g_data = $result;
+}
+catch (Eventarcapi_Exception $e)
+{
+	// Check $eventarc->error to see why this failed
+	// var_dump($eventarc->error);
+}	
+
+/**
+ * EXAMPLE eventarc.group.create
+ * 
+ * Lets create a group within the group we just 'got'. If g_id has not been set,
+ * (set within eventarc.group.list) then this method is going to explode and
+ * fail.
+ */
+
+// Lets put together some random stuff for this group
+$params = array(
+	'g_name'	=> 'group'.rand(1,500),
+	'g_parent'	=> $g_id,
+);
+
+try {
+	$result = $eventarc->call(
+		'eventarc.group.create',
+		$params);
+	$result = $eventarc->group_create($params);
+
+	// The result will contain the group information
+	$g_id = $result['g_id'];
+}
+catch (Eventarcapi_Exception $e)
+{
+	// Check $eventarc->error to see why this failed
+	// var_dump($e);
+}	
+
+/**
+ * EXAMPLE eventarc.event.create
+ * 
+ * Lets create a event within the group we just 'created'. 
+ * 
+ */
+
+// Lets make a default event
+$params = array(
+	'e_name' => 'My test event No. '.rand(1,500),
+	'e_description' => 'This is my event <strong>description</strong><img src="pic.jpg">',
+	'e_start' => date('Y-m-d G:i:s', time() + (7 * 24 * 60 * 60)), // Next week
+	'e_stop' => date('Y-m-d G:i:s', time() + (7 * 24 * 60 * 60) + 60), // Next week plus one minute
+	'e_deadline' => date('Y-m-d G:i:s', time() + (7 * 24 * 60 * 60) - 60), // Next week minus one minute
+	'e_status' => 'active', // Either active, draft or deleted
+	'e_timezone' => 'Australia/Melbourne', // If you omit this it will default to your users timezone
+	'e_craccccc' => 'fail',
+	'g_id' => $g_id,
+	'u_id' => $u_id,
+
+	// While we are at it, lets add some extra widgets. By default eventarc will
+	//  create a firstname, surname and email "widget" for the form. But you can
+	//  add other custom widgets quite easily too.
+
+	// Lets create a text field first
+	'0_wd_name' => 'my_custom_text_field',
+	'0_wd_type' => 'text',
+	'0_wd_label' => 'My text field',
+	'0_wd_description' => 'Please enter something into the text field...',
+	'0_wd_library' => 'W_custom',
+	'0_wd_validation' => 'required|length[1,100]',
+	'0_wd_order' => '10',
+
+	// Now lets create a dropdown box
+	'1_wd_name' => 'my_dropdown_box',
+	'1_wd_type' => 'dropdown',
+	'1_wd_label' => 'What you doing?',
+	'1_wd_description' => 'Please tell us all we want to know.',
+	'1_wd_library' => 'W_custom',
+	'1_wd_children'=> 'Yes|No|Maybe',
+	'1_wd_validation' => '',
+	'1_wd_order' => '11',
+
+);
+
+try {
+	$result = $eventarc->call(
+		'eventarc.event.create',
+		$params);
+
+	// The result will contain the group information
+	$e_data = $result;
+}
+catch (Eventarcapi_Exception $e)
+{
+	// Check $eventarc->error to see why this failed
+	// var_dump($e);
+}	
+
+// Lets make a default event
+$e_data = array(
+	'e_name' => 'My test event No. '.rand(1,500),
+	'e_description' => 'This is my event <strong>description</strong>',
+	'e_start' => date('Y-m-d G:i:s', time() + (7 * 24 * 60 * 60)), // Next week
+	'e_stop' => date('Y-m-d G:i:s', time() + (7 * 24 * 60 * 60) + 60), // Next week plus one minute
+	'e_deadline' => date('Y-m-d G:i:s', time() + (7 * 24 * 60 * 60) - 60), // Next week minus one minute
+	'e_status' => 'active', // Either active, draft or deleted
+	'e_timezone' => 'Australia/Melbourne', // If you omit this it will default to your users timezone
+	'g_id' => $g_id,
+	'u_id' => $u_id,
+	'e_pushurl' => 'http://myeventarc.cascade/ae/catcher'
+);
+
+// Add address
+$a_data = array(
+	'a_type' => 'venue',
+	'a_add1' => 'Floor 123, Suite 321',
+	'a_add2' => '123 Street Avenue',
+	'a_city' => 'Melbourne',
+	'a_state' => 'Victoria',
+	'a_post' => '3000',
+	'a_country' => 'Australia'
+);
+
+	// While we are at it, lets add some extra widgets. By default eventarc will
+	//  create a firstname, surname and email "widget" for the form. But you can
+	//  add other custom widgets quite easily too.
+$custom_text_widget = array(
+	// Lets create a text field first
+	'wd_name' => 'my_custom_text_field',
+	'wd_type' => 'text',
+	'wd_label' => 'My text field',
+	'wd_description' => 'Please enter something into the text field...',
+	'wd_library' => 'W_custom',
+	'wd_validation' => 'required|length[1,100]',
+	'wd_order' => '10',
+);
+
+$custom_dropdown_widget = array(
+	// Now lets create a dropdown box
+	'wd_name' => 'my_dropdown_box',
+	'wd_type' => 'dropdown',
+	'wd_label' => 'What you doing?',
+	'wd_description' => 'Please tell us all we want to know.',
+	'wd_library' => 'W_custom',
+	'wd_children'=> 'Yes|No|Maybe',
+	'wd_validation' => '',
+	'wd_order' => '11',
+);
+
+$pay_via_invoice_widget = array(
+	// Now lets create a dropdown box
+	'wd_name' => 'paybyinvoice',
+	'wd_type' => 'invisible',
+	'wd_label' => 'What you doing?',
+	'wd_description' => 'Please tell us all we want to know.',
+	'wd_library' => 'W_custom',
+	'wd_children'=> 'Yes|No|Maybe',
+	'wd_validation' => '',
+	'wd_order' => '11',
+);
+$general_ticket = array(
+	't_name' => 'Just a ticket',
+	't_description' => 'This is just a normal ticket',
+	't_total' => '1234',
+	't_price' => '0',
+	't_earlybird' => '0',
+	't_order' => '1',
+	't_type' => 'normal',
+	't_defaultquantity' => 10
+);
+
+$another_ticket = array(
+	't_name' => 'Some other ticket',
+	't_description' => 'This is just another normal ticket',
+	't_total' => '23',
+	't_price' => '0',
+	't_earlybird' => '0',
+	't_order' => '2',
+	't_type' => 'normal',
+	't_defaultquantity' => 10
+);
+
+$theme = array(
+	'th_body_bgcolor' => 'cc0000'
+);
+
+try {
+	$result = $eventarc
+		->add_event($e_data)
+		->add_address($a_data)
+		->add_widget($custom_text_widget)
+		->add_widget($custom_dropdown_widget)
+		->add_ticket_limit(2000) 
+		->add_ticket_show_fees(TRUE)
+		->add_ticket($general_ticket)
+		->add_ticket($another_ticket)
+		->add_theme($theme)
+		->event_create();
+
+	// The result will contain the group information
+	$e_data = $result;
+}
+catch (Eventarcapi_Exception $e)
+{
+	// Check $eventarc->error to see why this failed
+	// var_dump($e);
+}	
+
+/**
+ * EXAMPLE eventarc.event.list
+ * 
+ * Get all of our events
+ *
+ */
+
+// Because we set the u_id and u_apikey in the constructor, we don't need to
+//  worry about adding them in here as they are auto added.
+$params = array();
+
+try {
+//	$result = $eventarc->call(
+//		'eventarc.event.list',
+//		$params);
+
+	// The result will contain the event list
+	// var_dump($result);
+}
+catch (Eventarcapi_Exception $e)
+{
+	// Check $eventarc->error to see why this failed
+	// var_dump($eventarc->error);
+}	
+
+
+
+
+?>
+<!DOCTYPE html> 
+<html> 
+<head> 
+	<meta charset="UTF-8" /> 
+	<title>Eventarc api examples</title> 
+	
+	<link rel="shortcut icon" href="http://eventarc.com/favicon.ico" /> 
+	<link rel="icon" href="http://eventarc.com/images/favicon.ico" type="image/x-icon" /> 
+	<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.8/themes/base/jquery-ui.css" type="text/css" media="all" /> 
+	<link rel="stylesheet" href="http://static.jquery.com/ui/css/demo-docs-theme/ui.theme.css" type="text/css" media="all" /> 
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js" type="text/javascript"></script> 
+	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.8/jquery-ui.min.js" type="text/javascript"></script> 
+	<script src="http://jquery-ui.googlecode.com/svn/tags/latest/external/jquery.bgiframe-2.1.2.js" type="text/javascript"></script> 
+	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.8/i18n/jquery-ui-i18n.min.js" type="text/javascript"></script> 
+	<style type="text/css"> 
+		.startHidden {display:none;}
+	</style> 
+</head> 
+ 
+<body> 
+	<script>
+	$(function() {
+		$( "#accordion" ).accordion({
+			autoHeight: false	
+		});
+	});
+	</script>
+
+<div id="accordion">
+	<?php 
+	// Display results in accordion
+	$i = 1;
+	foreach ($eventarc->history as $api_call)
+	{
+		$error = FALSE;
+		if (array_key_exists('response', $api_call)
+			AND array_key_exists('error', $api_call['response']))
+		{
+			$result = ' : <span style="color:red">Failed</span>';
+			$error = TRUE;
+		}
+		else
+		{
+			$result = ' : <span style="color:green">Passed</span>';
+			$error = FALSE;
+		}
+		echo '<h3><a href="#">'.$api_call['method'].$result.'</a></h3>';
+		echo '<div>';
+		echo '<div id="tabs'.$i.'">
+			<ul>
+				<li><a href="#tabs'.$i.'-0">Method</a></li>
+				<li><a href="#tabs'.$i.'-1">Result</a></li>
+				<li><a href="#tabs'.$i.'-2">JSON result</a></li>
+				<li><a href="#tabs'.$i.'-3">Values sent</a></li>
+				<li><a href="#tabs'.$i.'-4">JSON sent</a></li>
+			</ul>';
+		echo '<div id="tabs'.$i.'-0" style="height:200px;"><p><pre>';
+		echo print_r($api_call['method'], TRUE);
+		echo '</pre></p></div>';
+		echo '<div id="tabs'.$i.'-1"><p><pre>';
+		echo print_r($api_call['response'], TRUE);
+		echo '</pre></p></div>';
+		echo '<div id="tabs'.$i.'-2"><p><pre>';
+		echo print_r($api_call['json_response'], TRUE);
+		echo '</pre></p></div>';
+		echo '<div id="tabs'.$i.'-3"><p><pre>';
+		echo print_r($api_call['payload'], TRUE);
+		echo '</pre></p></div>';
+		echo '<div id="tabs'.$i.'-4"><p><pre>';
+		echo print_r($api_call['json_payload'], TRUE);
+		echo '</pre></p></div>';
+		echo '<script> $(function() { $( "#tabs'.$i.'" ).tabs(); 	});	</script></div>';
+		echo '</div>';
+		$i++;
+	}
+
+
+	?>
+	</div>
+</body>
+</html>
+
